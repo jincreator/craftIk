0f6f5957a421bc30e843e838880183a4345594f5
Jinkyu Yi <jincreator@jincreator.net>
2012-09-04 23:27:18 +0900

    Use autotools and modify some code to make it able to compile in Linux.

--- ProcessManager/ProcessManager.c
+++ ProcessManager/ProcessManager.c
@@ -6,6 +6,12 @@
 //  Copyright (c) 2012 SI Cyrusian. All rights reserved.
 //
 
+#include <stdio.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 int main(int argc, const char **argv) {
 	return 0;
 }
